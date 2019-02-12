b4a97cc5c611537fc251f70771af697bd6578abb
prabhu-durasoft <iamprabhu@yahoo.com>
2019-02-12 12:21:48 +0530

    Day01

--- Day01/Lab03.js
+++ Day01/Lab03.js
@@ -10,3 +10,34 @@ greatest(21,65);
 greatest(1,55,83,9,667);
 
 */
+
+const add = function() {
+	let sum = 0;
+	for(let i=0; i<arguments.length; i++) {
+		if(typeof(arguments[i]) == 'number') {
+			sum += arguments[i];	
+		}
+	}
+	console.log(`Sum: ${sum}`);
+};
+
+add(1,5,34,324,324,235,2353);
+add(21,65);
+add(1,55,83);
+
+const greatest = function() {
+	let max = arguments.length > 0 ? arguments[0] : -1;
+	for(let i=0; i<arguments.length; i++) {
+		if(typeof(arguments[i]) == 'number') {
+			max = arguments[i] > max ? arguments[i] : max;
+		}	
+	}
+	console.log(`Max: ${max}`);	
+};
+
+greatest(1,5,34,324,324,235,2353);
+greatest(21,65);
+greatest(1,55,83,9,667);
+
+
+
