2d87e7d4d6351331b0cdeb4860f09b60b9154c43
prabhu-durasoft <iamprabhu@yahoo.com>
2019-02-12 11:47:30 +0530

    Day01

--- Day01/Lab02.js
+++ Day01/Lab02.js
@@ -12,3 +12,30 @@ Iterate the array and print the array values in a  querystring format like this.
 
 ?name=Sam&age=12&country=India&city=Bengaluru
 */
+let arr = [];
+arr['name'] = 'Sam';
+arr['age'] = 12;
+arr['country'] = 'India';
+arr['city'] = 'Bengaluru';
+
+let queryString = '?';
+for(let key in arr) {
+	queryString += key + '=' + arr[key] + '&';
+}
+queryString = queryString.substring(0, queryString.length - 1);
+console.log(queryString);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
