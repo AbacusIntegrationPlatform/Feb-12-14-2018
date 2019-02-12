c236c17c9478b645795c5eca89b0bbcf6b16b4d6
prabhu-durasoft <iamprabhu@yahoo.com>
2019-02-12 11:33:23 +0530

    Day01

---
+++ Day01/Lab02.js
@@ -0,0 +1,14 @@
+/* Let's warm-up.
+Create Lab02.js file. 
+Implement the following and run it using Node.
+
+1) Create an array that has the following key-values
+name  Sam
+age   12
+country India
+city Bengaluru
+
+Iterate the array and print the array values in a  querystring format like this.
+
+?name=Sam&age=12&country=India&city=Bengaluru
+*/
