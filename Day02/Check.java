c35c44ce7bb9f1b538955feea7d7d30cac3c58ff
prabhu-durasoft <iamprabhu@yahoo.com>
2019-02-13 12:27:31 +0530

    Day02

---
+++ Day02/Check.java
@@ -0,0 +1,15 @@
+public class Check {
+	public static void main(String[] args) {
+		var i = 10;
+		i = "somrthing";
+		System.out.println(i);
+		String str = "";
+		System.out.println(str.isBlank());
+		// if(str != null && !"".equals(str)) {
+// 			System.out.println(str);
+// 		}
+// 		else {
+// 			System.out.println("str is blank");
+// 		}
+	}
+}	
